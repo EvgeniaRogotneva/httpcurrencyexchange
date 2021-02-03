32b2e29434ef308bbee5ef1467fa1f1e3a81643f
evgeniya rogotneva <murka_nnov@bk.ru>
2021-02-03 15:11:24 +0300

    add file

--- main.py
+++ main.py
@@ -4,34 +4,33 @@ from pydantic import BaseModel
 storage = {}
 app = FastAPI()
 
-@app.get('/')
-def home():
-    return {'key':'hello'}
-
-@app.get('/{pk}')
-def get_item(pk:int, q: int=None):
-    return {'key': pk, 'q': q}
-
-@app.get('/currency/{currency_code}/date/{currency_date}/')
-def get_currency(currency_code:str, date: datetime.date=datetime.datetime.now().date()):
-    return storage[currency_code][date]
 
 class CurrencyRate(BaseModel):
     rate: float
-    currency_date: datetime.date=datetime.datetime.now().date()
+    currency_date: datetime.date = datetime.date.today()
 
 
-@app.post('/currency/{currency_code}')
-def post(currency_code:str, currency_rate: CurrencyRate):
-    print('currency_code', currency_code)
-    print('currency_rate.currency_date', currency_rate.currency_date)
-    print('currency_rate.rate', currency_rate.rate)
-    storage[currency_code][currency_rate.currency_date] = currency_rate.rate
-    return  currency_rate
+@app.post('/post/currency/{currency_code}')
+def post(currency_code: str, currency_rate: CurrencyRate):
+    storage[currency_code] = {currency_rate.currency_date: currency_rate.rate}
+    return storage[currency_code]
 
 
+@app.get('/get/currency/{currency_code}/')
+@app.get('/get/currency/{currency_code}/date/{date}/')
+def get_currency(currency_code: str, date: datetime.date = datetime.date.today()):
+    if currency_code in storage and date in storage[currency_code]:
+        return storage[currency_code][date]
+    else:
+        return 'Sorry, there is no info about' + currency_code + date + 'rate'
 
 
-
-
-
+@app.get('/get/from/{from_currency_code}/to/{to_currency_code}/')
+@app.get('/get/from/{from_currency_code}/to/{to_currency_code}/date/{date}')
+def get_rate_for_pair(from_currency_code: str, to_currency_code: str, date: datetime.date = datetime.date.today()):
+    if from_currency_code in storage and to_currency_code in storage:
+        if date in storage[from_currency_code] and date in storage[to_currency_code]:
+            rate = storage[from_currency_code][date] / storage[to_currency_code][date]
+            return rate
+    else:
+        return 'I do not have enough information for your request'
