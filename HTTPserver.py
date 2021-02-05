ae2e592dc1e2e6d0215652a22521e26417b0d081
evgeniya rogotneva <murka_nnov@bk.ru>
2021-02-05 14:36:05 +0300

    add tests

--- HTTPserver.py
+++ HTTPserver.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import datetime
 from pydantic import BaseModel
+from all_currencies import all_currencies
 storage = {}
 app = FastAPI()
 
@@ -12,24 +13,26 @@ class CurrencyRate(BaseModel):
 
 @app.get('/')
 async def read_main():
-    return {"msg": "Hello World"}
+    return {"msg": "Hi! I am little but very myself proud HTTP currency exchange server"}
+
 
 @app.post('/post/currency/{currency_code}')
 def post(currency_code: str, currency_rate: CurrencyRate):
-    if currency_rate.rate > 0 and isinstance(currency_code, str):
+    if currency_rate.rate > 0 and currency_code in all_currencies:
         storage[currency_code] = {currency_rate.currency_date: currency_rate.rate}
-        return {'currency_code': currency_code, 'currency_rate': currency_rate}
+        return {'currency_code': currency_code, 'currency_rate': currency_rate.rate, 'date': currency_rate.currency_date}
     else:
-        return {'error': 'Currency rate should be bigger than 0, currency_code should be string'}
+        return {'error': 'Currency rate should be bigger than 0, currency_code should be in follow format: '
+                         'AUD, RUB, USD, EUR'}
 
 
 @app.get('/get/currency/{currency_code}/')
 @app.get('/get/currency/{currency_code}/date/{date}/')
 def get_currency(currency_code: str, date: datetime.date = datetime.date.today()):
     if currency_code in storage and date in storage[currency_code]:
-        return storage[currency_code][date]
+        return {'currency_code': currency_code, 'currency_rate': storage[currency_code][date], 'date': date}
     else:
-        return 'Sorry, there is no info about ' + currency_code + ' ' + str(date) + ' rate'
+        return {'error': 'Sorry, there is no info about ' + currency_code + ' ' + str(date) + ' rate'}
 
 
 @app.get('/get/from/{from_currency_code}/to/{to_currency_code}/')
@@ -38,6 +41,6 @@ def get_rate_for_pair(from_currency_code: str, to_currency_code: str, date: date
     if from_currency_code in storage and to_currency_code in storage:
         if date in storage[from_currency_code] and date in storage[to_currency_code]:
             rate = storage[from_currency_code][date] / storage[to_currency_code][date]
-            return rate
+            return {'1 ' + from_currency_code: str(rate) + ' ' + to_currency_code, 'date': date}
     else:
-        return 'I do not have enough information for your request'
+        return {'error': 'I do not have enough information for your request'}
--- test.py
+++ test.py
@@ -1,66 +1,141 @@
 import unittest
 from HTTPserver import app
+from HTTPserver import storage
 from starlette.testclient import TestClient
 import requests
-
-
+import datetime
 
 
 class TestCurrencyExchange(unittest.TestCase):
+
     def setUp(self) -> None:
         pass
 
-
+    def tearDown(self) -> None:
+        storage = {}
+        print('we are here')
 
     def test_get_main(self):
         with TestClient(app) as client:
             response = client.get('/')
             assert response.status_code == 200
-            assert response.json() == {"msg": "Hello World"}
+            assert response.json() == {"msg": "Hi! I am little but very myself proud HTTP currency exchange server"}
 
     def test_add_currency(self):
         with TestClient(app) as client:
             response = client.post("/post/currency/AUD", json={
                                     "rate": 58.0284,
                                     "currency_date": "2021-02-03"})
-            print(response)
-            print(response.status_code)
-            print(response.json())
-
-
-
-
-    def test_add_currency_with_date(self):
-        pass
-
+            assert response.status_code == 200
+            assert response.json() == {"currency_code": "AUD", "currency_rate": 58.0284, "date": "2021-02-03"}
+        self.tearDown()
 
     def test_get_currency_rate(self):
-        pass
-
+        with TestClient(app) as client:
+            response = client.post("/post/currency/AUD", json={
+                "rate": 58.0284,
+                "currency_date": str(datetime.date.today())})
+            assert response.status_code == 200
+            response = client.get('/get/currency/AUD')
+            assert response.status_code == 200
+            assert response.json() == {"currency_code": "AUD", "currency_rate": 58.0284,
+                                       "date": str(datetime.date.today())}
+        self.tearDown()
 
     def test_get_currency_rate_with_date(self):
-        pass
-
+        with TestClient(app) as client:
+            response = client.post("/post/currency/AUD", json={
+                "rate": 58.0284,
+                "currency_date": "2021-02-03"})
+            assert response.status_code == 200
+            response = client.get('/get/currency/AUD/date/2021-02-03')
+            assert response.status_code == 200
+            assert response.json() == {"currency_code": "AUD", "currency_rate": 58.0284,
+                                       "date": "2021-02-03"}
+        self.tearDown()
 
     def test_get_rate_for_pair(self):
-        pass
+        with TestClient(app) as client:
+            '''post currency's rates'''
+            response = client.post("/post/currency/EUR", json={"rate": 91.5624,
+                                                               "currency_date": str(datetime.date.today())})
+            assert response.status_code == 200
+            response = client.post("/post/currency/USD", json={"rate": 76.0801,
+                                                               "currency_date": str(datetime.date.today())})
+            assert response.status_code == 200
+
+            '''get currency's rate'''
+            response = client.get('/get/from/EUR/to/USD')
+            assert response.status_code == 200
+            assert response.json() == {'1 EUR': '1.2034999953995853 USD', 'date': str(datetime.date.today())}
+            response = client.get('/get/from/USD/to/EUR')
+            assert response.status_code == 200
+            assert response.json() == {'1 USD': '0.8309098494578561 EUR', 'date': str(datetime.date.today())}
+        self.tearDown()
+
+    def test_get_rate_for_pair_with_date(self):
+        with TestClient(app) as client:
+            '''post currency's rates'''
+            response = client.post("/post/currency/EUR", json={"rate": 91.5624,
+                                                               "currency_date": "2021-02-05"})
+            assert response.status_code == 200
+            response = client.post("/post/currency/USD", json={"rate": 76.0801,
+                                                               "currency_date": "2021-02-05"})
+            assert response.status_code == 200
+
+            '''get currency's rate for pair with date'''
+            response = client.get('/get/from/EUR/to/USD/date/2021-02-05')
+            assert response.status_code == 200
+            assert response.json() == {'1 EUR': '1.2034999953995853 USD', 'date': '2021-02-05'}
+            response = client.get('/get/from/USD/to/EUR/date/2021-02-05')
+            assert response.status_code == 200
+            assert response.json() == {'1 USD': '0.8309098494578561 EUR', 'date': '2021-02-05'}
+
+        self.tearDown()
+
 
 
     def test_add_currency_wrong_format(self):
-        pass
-
+        with TestClient(app) as client:
+            response = client.post("/post/currency/123", json={
+                                    "rate": 58.0284,
+                                    "currency_date": "2021-02-03"})
+            assert response.status_code == 200
+            assert response.json() == {'error': 'Currency rate should be bigger than 0, '
+                                                'currency_code should be in follow format: AUD, RUB, USD, EUR'}
+        self.tearDown()
 
     def test_get_currency_rate_from_empty_server(self):
-        pass
+        with TestClient(app) as client:
+            response = client.get('/get/currency/AUD')
+            assert response.status_code == 200
+            print(response.json())
+            assert response.json() == {'error': 'Sorry, there is no info about AUD '
+                                                + str(datetime.date.today()) + ' rate'}
+        self.tearDown()
 
+    def test_get_currency_rate_with_date_from_empty_server(self):
+        with TestClient(app) as client:
+            response = client.get('/get/currency/AUD/date/2021-02-03')
+            assert response.status_code == 200
+            assert response.json() == {'error': 'Sorry, there is no info about AUD 2021-02-03 rate'}
+        self.tearDown()
 
     def test_get_rate_for_pair_from_empty_server(self):
-        pass
-
+        with TestClient(app) as client:
+            '''get currency's rate'''
+            response = client.get('/get/from/EUR/to/USD')
+            assert response.status_code == 200
+            assert response.json() == {'error': 'I do not have enough information for your request'}
+        self.tearDown()
 
     def test_post_zero_rate(self):
-        pass
+        with TestClient(app) as client:
+            response = client.post("/post/currency/AUD", json={
+                "rate": 0,
+                "currency_date": "2021-02-03"})
+            assert response.status_code == 200
+            assert response.json() == {'error': 'Currency rate should be bigger than 0, '
+                                                'currency_code should be in follow format: AUD, RUB, USD, EUR'}
+        self.tearDown()
 
-
-    def tearDown(self) -> None:
-        pass
